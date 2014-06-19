3098ba3594a6cc27cab2513d3470efa55595f154
Farzad Noorian <farzad.noorian@gmail.com>
2014-06-19 16:51:06 +1000

    Improved print function's styling

--- R/print.grammar.R
+++ R/print.grammar.R
@@ -1,8 +1,12 @@
-print.grammar <-
-function(x, ...) {
+print.grammar <- function (x, ...) {
+  maxLen = max(sapply (x$def, function(rule) nchar(rule[[1]])))
   
   for (rule in x$def) {
-    cat(paste0('<', rule[[1]], ">:"), unlist(lapply(rule[[2]], function(x) paste0('"', x, '"'))), "\n", ...)
+    spaces = maxLen + 1 - nchar(rule[[1]])  # use spaces as tabs usually don't work
+    cat(paste0("<", rule[[1]], ">:"), 
+        do.call(paste0, as.list(rep(" ", spaces))),
+        unlist(lapply(rule[[2]], function(x) paste0("\"", x, "\""))), 
+        "\n", ...)
   }
 }
 
