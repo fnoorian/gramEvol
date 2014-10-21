9dc2b9d950cefb62f96b28e88a8ac9fbf38490e6
Farzad Noorian <farzad.noorian@gmail.com>
2014-10-21 12:58:59 +1100

    Grammar printing style improved

--- R/print.grammar.R
+++ R/print.grammar.R
@@ -2,12 +2,12 @@ print.grammar <- function (x, ...) {
   maxLen = max(sapply (x$def, function(rule) nchar(rule[[1]])))
   
   for (rule in x$def) {
-    spaces = maxLen + 1 - nchar(rule[[1]])  # use spaces as tabs usually don't work
-    cat(paste0("<", rule[[1]], "> ::="), 
+    spaces = maxLen - nchar(rule[[1]])  # use spaces as tabs usually don't work
+    expressions = lapply(rule[[2]], function(x) x)
+    cat(paste0("<", rule[[1]], ">" ), 
         do.call(paste0, as.list(rep(" ", spaces))),
-        unlist(lapply(rule[[2]], function(x) paste0("\"", x, "\""))), 
+        "::= ",
+        do.call(function(...) {paste(..., sep=" | ")}, expressions), 
         "\n", ...)
   }
 }
-
-
