e8ba463008203da21378f9e4aa9c06812d66889d
Farzad Noorian <farzad.noorian@gmail.com>
2014-10-21 12:04:53 +1100

    BNF printing style improved

--- R/print.grammar.R
+++ R/print.grammar.R
@@ -3,7 +3,7 @@ print.grammar <- function (x, ...) {
   
   for (rule in x$def) {
     spaces = maxLen + 1 - nchar(rule[[1]])  # use spaces as tabs usually don't work
-    cat(paste0("<", rule[[1]], ">:"), 
+    cat(paste0("<", rule[[1]], "> ::="), 
         do.call(paste0, as.list(rep(" ", spaces))),
         unlist(lapply(rule[[2]], function(x) paste0("\"", x, "\""))), 
         "\n", ...)
