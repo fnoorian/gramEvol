2cdb6f34daa065844d01166001662534102b516f
Farzad Noorian <farzad.noorian@gmail.com>
2015-04-13 13:42:56 +1000

    Added Grammar Rule Concatenation

--- R/GERule_Concat.R
+++ R/GERule_Concat.R
@@ -13,8 +13,10 @@ c.GERule <- function(..., recursive = FALSE) {
   l = list(...)
   
   ret = l[[1]]
-  for (i in 2:length(l)) {
-    ret = GERule.Concat(ret, l[[i]])
+  if (length(l) > 1) {
+    for (i in 2:length(l)) {
+      ret = GERule.Concat(ret, l[[i]])
+    }
   }
   
   ret
